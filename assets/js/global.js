e37a1958486bb61885b33c1a6a7a3715a1bd7b70
therangel <229519868+therangel@users.noreply.github.com>
2026-03-19 12:06:45 -0300

    Change theme added

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,33 +1,17 @@
 
 /* =============== ELEMENTS ================ */
 
-const darkBtn = document.querySelector(".dark-btn")
-const lightBtn = document.querySelector(".light-btn")
-const main = document.querySelector(".main")
+const lightIcon = document.querySelector(".fa-sun")
+const darkIcon = document.querySelector(".fa-moon")
+const themeIcon = document.querySelector(".toggle-theme")
 // const sideBar = document.querySelector(".sidebar")
 
-
-darkBtn.addEventListener("click", () => {
-   ligthTheme()
+themeIcon.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme")
+    lightIcon.classList.toggle("themeChange")
+    darkIcon.classList.toggle("themeChange")
 })
 
-lightBtn.addEventListener("click", () => {
-   darkTheme()
-})
-
-function ligthTheme() {
-    darkBtn.classList.add("hidden") 
-    lightBtn.classList.add("visible") 
-    main.classList.add("light")
-    sideBar.classList.add("light")
-}
-
-function darkTheme() {
-    darkBtn.classList.remove("hidden") 
-    lightBtn.classList.remove("visible") 
-    main.classList.remove("light")
-    sideBar.classList.remove("light")
-}
 
 
 
